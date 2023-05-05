4b4aa09a43239e6404083e866aa21d8375f2645d
MohamedSaidCS <60986229+MohamedSaidCS@users.noreply.github.com>
2023-05-06 01:54:42 +0300

    move icons in assets folder, loading state

--- src/components/Bookshelf/Bookshelf.tsx
+++ src/components/Bookshelf/Bookshelf.tsx
@@ -4,19 +4,24 @@ import BookCard from '../BookCard/BookCard';
 
 interface BookShelfProps {
 	title: string;
+	isLoading: boolean;
 	books: Book[];
 }
 
-const Bookshelf: FC<BookShelfProps> = ({ title, books }) => {
+const Bookshelf: FC<BookShelfProps> = ({ title, isLoading, books }) => {
 	return (
 		<div className='bookshelf'>
 			<h2 className='bookshelf-title'>{title}</h2>
 			<div className='bookshelf-books'>
-				<ol className='books-grid'>
-					{books.map((book) => (
-						<BookCard key={book.id} book={book} />
-					))}
-				</ol>
+				{isLoading ? (
+					<div>Loading...</div>
+				) : (
+					<ol className='books-grid'>
+						{books.map((book) => (
+							<BookCard key={book.id} book={book} />
+						))}
+					</ol>
+				)}
 			</div>
 		</div>
 	);
--- src/pages/Books/Books.tsx
+++ src/pages/Books/Books.tsx
@@ -5,40 +5,38 @@ import { Link } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import Bookshelf from '../../components/Bookshelf/Bookshelf';
 
+type Books = { currentlyReading: Book[]; wantToRead: Book[]; read: Book[] };
+
+const shelves: { title: string; key: keyof Books }[] = [
+	{ title: 'Currently Reading', key: 'currentlyReading' },
+	{ title: 'Want to Read', key: 'wantToRead' },
+	{ title: 'Read', key: 'read' },
+];
+
 const Books = () => {
 	const { data, isLoading, isError } = useQuery({
 		queryKey: ['books'],
 		queryFn: getAllBooks,
 		select: (data) =>
-			data.reduce(
+			data.reduce<Books>(
 				(accumulator, currentValue) => {
 					accumulator[currentValue.shelf].push(currentValue);
 					return accumulator;
 				},
-				{
-					currentlyReading: [],
-					wantToRead: [],
-					read: [],
-				} as {
-					currentlyReading: Book[];
-					wantToRead: Book[];
-					read: Book[];
-				}
+				{ currentlyReading: [], wantToRead: [], read: [] }
 			),
 	});
 
-	if (isLoading || isError) return <div>Loading or Error</div>;
+	if (isError) return <div>Error</div>;
 
 	return (
 		<div className='app'>
 			<div className='list-books'>
 				<Header />
 				<div className='list-books-content'>
-					<div>
-						<Bookshelf title='Currently Reading' books={data.currentlyReading} />
-						<Bookshelf title='Want To Read' books={data.wantToRead} />
-						<Bookshelf title='Read' books={data.read} />
-					</div>
+					{shelves.map((shelf) => (
+						<Bookshelf key={shelf.key} title={shelf.title} isLoading={isLoading} books={isLoading ? [] : data[shelf.key]} />
+					))}
 				</div>
 				<div className='open-search'>
 					<Link to={'/search'}>Add a book</Link>
